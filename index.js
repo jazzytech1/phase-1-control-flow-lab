f56a83970f9e62ce27865857a6726d3fe46161a4
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-11 23:02:48 -0400

    commit tree 597c15e5d1c4637300ef6cb3ab06305cf289b62c

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+if (ride <=400){
+return 'This one is on me!'
+}else if (1999 < ride && ride < 2500){ //ride is nested which allows the falsey and the truthy statements to be executed
+return "I will gladly take your thirty bucks."
+}else{
+  return "No can do."
+}
+}
+function ternaryCheckCity(NYC){
+  let city = NYC
+return city==='NYC'?'Ok, sounds good.' : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+switch (tip){
+  case 'generous':
+  return "Thank you so much."
+
+  case 'not as generous':
+  return "Thank you."
+
+  default: 'if anything else'
+  return "Bye."
+
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+
